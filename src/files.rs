65c8e79c3115e40d7a8839997ed0641eafef1615
Jegor Kitskerkin <jegor.kitskerkin@gmail.com>
2023-11-29 19:22:19 +0200

    Split functions.rs

--- src/files.rs
+++ src/files.rs
@@ -1,8 +1,6 @@
-use crate::analysis_error::AnalysisError;
-use parsed_python_file::ParsedPythonFile;
+use std::{fs, path::PathBuf};
+
 use python_file::PythonFile;
-use rustpython_ast::{ArgWithDefault, StmtFunctionDef};
-use std::{collections::HashMap, fs, path::PathBuf};
 pub mod parsed_python_file;
 pub mod python_file;
 
@@ -46,79 +44,6 @@ pub fn get_files_list(provided: Vec<PathBuf>, recursive: bool) -> Vec<PathBuf> {
             }
         })
         .collect()
-}
-
-pub fn get_argument_annotation(arg: &ArgWithDefault) -> Option<String> {
-    Some(arg.def.annotation.clone()?.as_name_expr()?.id.to_string())
-}
-
-pub fn get_return_annotation(func: &StmtFunctionDef) -> Option<String> {
-    match &func.returns {
-        Some(returns) => Some(returns.as_name_expr()?.id.to_string()),
-        None => None,
-    }
-}
-
-pub fn check_function_arguments(
-    func: &StmtFunctionDef,
-    fixtures: &HashMap<String, StmtFunctionDef>,
-) -> Vec<AnalysisError> {
-    let function_name = &func.name;
-
-    let mut errors: Vec<AnalysisError> = Vec::new();
-
-    for arg in func.args.args.iter() {
-        let arg_name = arg.def.arg.to_string();
-        let arg_annotation = get_argument_annotation(arg);
-
-        match arg_annotation {
-            Some(arg_annotation) => {
-                let fixture = fixtures.get(&arg_name);
-                match fixture {
-                    Some(fixture) => {
-                        if let Some(fixture_annotation) = get_return_annotation(fixture) {
-                            if fixture_annotation != arg_annotation {
-                                errors.push(AnalysisError::IncorrectArgumentType {
-                                    function_name: function_name.to_string(),
-                                    argument_name: arg_name,
-                                    expected_type: fixture_annotation,
-                                    provided_type: arg_annotation,
-                                })
-                            }
-                        }
-                    }
-                    None => errors.push(AnalysisError::FixtureDoesNotExist {
-                        function_name: function_name.to_string(),
-                        argument_name: arg_name,
-                    }),
-                }
-            }
-            None => errors.push(AnalysisError::MissingArgumentType {
-                function_name: function_name.to_string(),
-                argument_name: arg_name,
-            }),
-        }
-    }
-    errors
-}
-
-pub fn check_file(file: &ParsedPythonFile) -> Vec<AnalysisError> {
-    let mut errors = Vec::new();
-
-    for (fixture_name, func) in file.fixtures.iter() {
-        if get_return_annotation(func).is_none() {
-            errors.push(AnalysisError::FixtureMissingReturnType {
-                fixture_name: fixture_name.clone(),
-            })
-        }
-        errors.extend(check_function_arguments(func, &file.fixtures))
-    }
-
-    for (_test_case_name, func) in file.test_cases.iter() {
-        errors.extend(check_function_arguments(func, &file.fixtures))
-    }
-
-    errors
 }
 
 #[cfg(test)]
